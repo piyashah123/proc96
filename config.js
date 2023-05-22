0d7c4a65cad7e4be2bfaa0be61543ab40f8847f3
piyashah123 <82143407+piyashah123@users.noreply.github.com>
2023-05-22 12:48:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDgZs8UrVtGLO540MkAxJXjbaWEcsFHgzI",
+    authDomain: "musicapp-aad0a.firebaseapp.com",
+    projectId: "musicapp-aad0a",
+    storageBucket: "musicapp-aad0a.appspot.com",
+    messagingSenderId: "904592637220",
+    appId: "1:904592637220:web:99faa538d93e6a4fe047b1"
+  };
+  
+  // Initialize Firebase
+ // const app = initializeApp(firebaseConfig);
